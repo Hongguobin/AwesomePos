1533bc882c5e97284a6e828f71fedfd0eaf82c01
Hongguobin <1304098549@qq.com>
2019-04-09 16:16:08 +0800

    收银系统

---
+++ src/components/common/leftNav.vue
@@ -0,0 +1,59 @@
+<template>
+	<div class="left-nav">
+		<ul>
+			<li>
+				<i class="icon iconfont icon-wode"></i>
+				<div>收银</div>
+			</li>
+			<li>
+				<i class="icon iconfont icon-xihuan"></i>
+				<div>店铺</div>
+			</li>
+			<li>
+				<i class="icon iconfont icon-dianzan"></i>
+				<div>商品</div>
+			</li>
+			<li>
+				<i class="icon iconfont icon-xingji"></i>
+				<div>会员</div>
+			</li>
+			<li>
+				<i class="icon iconfont icon-xiaoxi"></i>
+				<div>统计</div>
+			</li>
+			<!-- <li>
+				<i class="icon iconfont icon-xiaoxi"></i>
+				<div>设置</div>
+			</li> -->
+		</ul>
+	</div>
+</template>
+<script>
+	export default {
+		
+	}
+</script>
+<style>
+	 .left-nav{
+       color:#fff;
+       font-size:10px;
+       height:100%;
+       background-color: #1D8ce0;
+       float:left;
+       width:5%;
+	   
+    }
+    .iconfont{
+       font-size:24px;
+    }
+    .left-nav ul{
+        padding:0px;
+        margin: 0px;
+    }
+    .left-nav li{
+        list-style: none;
+        text-align: center;
+        border-bottom:1px solid #20a0ff;
+        padding:10px;
+    }
+</style>
